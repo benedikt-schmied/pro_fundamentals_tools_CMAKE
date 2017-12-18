7f39c096e85dbbe2875b26fa0a1ed103a687d06a
Benedikt Schmied <github-ce@benedikt-schmied.de>
2017-12-18 16:46:49 +0100

    compiled header file

--- first_steps/tutorial.cxx
+++ first_steps/tutorial.cxx
@@ -2,9 +2,13 @@
  * some simple lines in order to get familiar with CMake
  */
 
+/* c - runtime */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+/* project */
+#include "TutorialConfig.h"
 
 int main(int argc, char *argv[]) 
 {
@@ -15,6 +19,11 @@ int main(int argc, char *argv[])
 
 	/* check, wether we've got enough valid parameters */
 	if (argc < 2) {
+		fprintf(stdout, "%s Version %d.%d\n", 
+			argv[0],
+			Tutorial_VERSION_MAJOR,
+			Tutorial_VERSION_MINOR);
+		
 		fprintf(stdout, "Usage: %s number \n", argv[0]);
 		return 1;
 	}
