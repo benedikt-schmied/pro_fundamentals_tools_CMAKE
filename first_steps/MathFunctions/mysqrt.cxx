6e8b48d7bd8ebbd45eafd4307cd35f02f2e06854
Benedikt Schmied <github-ce@benedikt-schmied.de>
2018-08-01 15:18:05 +0200

    some new files

--- first_steps/MathFunctions/mysqrt.cxx
+++ first_steps/MathFunctions/mysqrt.cxx
@@ -1,4 +1,15 @@
+#include "Table.h"
+
 float mysqrt(float _val)
 {
-	return _val;
+	/* automatic variables */
+	double result;
+
+	/* executable statements */
+	result = _val;
+	if (_val >= 1 && _val < 10) {
+		result = sqrtTable[static_cast<int>(_val)];
+	}	
+
+	return result;
 }
