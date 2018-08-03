07b313a49da00d572606f457e54465819c89fbf9
Benedikt Schmied <development@benedikt-schmied.de>
2018-08-03 20:19:42 +0200

    linking works

--- second_step/_pro/main.c
+++ second_step/_pro/main.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
+#include <stddef.h>
 
-#include <itg_lib1gnc.h>
+#include <itg_lib1_gnc.h>
 
-int main(int argc, char *argv[]}
+int main(int argc, char *argv[])
 {
+	itg_lib1_gnc__init(NULL);
 	return 0;
 }
